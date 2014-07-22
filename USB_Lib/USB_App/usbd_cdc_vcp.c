e76d2650c0c1d16583af3e2eacf7a8cf0a3ea0e1
RdMaxes <yc.huang0522@gmail.com>
2014-07-22 21:26:46 +0800

    determine if Len need to be minused one

--- USB_Lib/USB_App/usbd_cdc_vcp.c
+++ USB_Lib/USB_App/usbd_cdc_vcp.c
@@ -186,7 +186,9 @@ uint16_t VCP_DataRx(uint8_t* Buf, uint32_t Len)
    uint32_t i;
    if(Len>1) //Len bigger than 1 means data exisit
    {
-      if(*(Buf+Len-1)=='\r') Len--;
+      if((Len==64)&&(*(Buf+Len-1)=='\r')) Len--; //delete '\r' byte
+      else if (Len!=64) Len--;
+
       for (i = 0; i < Len; i++) 
       {
          USB_RxBuffer[usb_rx_data_pos] = *(Buf + i);
